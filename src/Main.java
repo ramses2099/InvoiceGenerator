62f8d01322711ea54d5209e40c56b350af3c42c6
ramses2099 <ramses2099@gmail.com>
2024-01-03 14:26:34 -0400

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import form.*;
+
+import java.awt.*;
+import java.io.InputStream;
+import java.net.URL;
 
 public class Main {
 
@@ -12,7 +17,15 @@ public class Main {
                 frmMain.setVisible(true);
                 frmMain.setTitle("Invoice Generator");
                 frmMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frmMain.setResizable(false);
+                frmMain.setLocationRelativeTo(null);
                 frmMain.setBounds(100, 100, 931, 702);
+
+                try {
+                    frmMain.setIconImage(ImageIO.read(getClass().getResourceAsStream ("/images/Zoom.png")));
+                }catch (Exception ex){
+                    System.out.println(ex.getMessage());
+                }
             }
         });
     }
--- src/com/dpworld/Utils.java
+++ src/com/dpworld/Utils.java
@@ -1,5 +1,8 @@
 package com.dpworld;
 
+import javassist.Loader;
+
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -13,4 +16,33 @@ public class Utils {
         }
         return rs;
     }
+
+    public static URL getResource(String resource){
+
+        URL url ;
+
+        //Try with the Thread Context Loader.
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        if(classLoader != null){
+            url = classLoader.getResource(resource);
+            if(url != null){
+                return url;
+            }
+        }
+
+        //Let's now try with the classloader that loaded this class.
+        classLoader = Loader.class.getClassLoader();
+        if(classLoader != null){
+            url = classLoader.getResource(resource);
+            if(url != null){
+                return url;
+            }
+        }
+
+        //Last ditch attempt. Get the resource from the classpath.
+        return ClassLoader.getSystemResource(resource);
+    }
+
+
+
 }
